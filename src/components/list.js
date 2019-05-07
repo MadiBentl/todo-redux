93c848a9a9b03d1ed0d00ffa3da09075f4562ef0
Madison Bentley <mb.madisonbentley@gmail.com>
2019-05-07 08:50:55 -0700

    added checkboxes and made list data display preoprely

--- src/components/list.js
+++ src/components/list.js
@@ -3,11 +3,23 @@ import {connect} from 'react-redux';
 
 class List extends Component {
   render() {
-    console.log(this.props);
+    const items = [];
+    this.props.list.forEach(item =>{
+      items.push(
+        <div className="field">
+          <div className="ui checkbox">
+            <input type="checkbox" key={item.task} name={item.task} />
+            <label>{item.task}</label>
+          </div>
+        </div>
+      )
+    });
     return (
       <div>
         <h3>List</h3>
-        <p>List Length: 0</p>
+        {items}
+        <p>Total Items: {items.length}</p>
+        <p>Items To Do: 0 </p>
         <p>Items Finished: 0</p>
       </div>
     );
