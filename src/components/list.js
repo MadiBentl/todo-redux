e6ce12d330e57a3c6265c31635637f75dc342469
Madison Bentley <mb.madisonbentley@gmail.com>
2019-05-10 08:54:05 -0700

    removed total items and added it as a fraction to other data points

--- src/components/list.js
+++ src/components/list.js
@@ -31,9 +31,8 @@ class List extends Component {
           <h3>List</h3>
           {items}
         </div>
-        <p>Total Items: {items.length}</p>
-        <p>Items To Do: {itemsNotDone.length} </p>
-        <p>Items Finished: {items.length - itemsNotDone.length}</p>
+        <p>Items To Do: {itemsNotDone.length}/{items.length}</p>
+        <p>Items Finished: {items.length - itemsNotDone.length}/{items.length}</p>
       </div>
     );
   }
